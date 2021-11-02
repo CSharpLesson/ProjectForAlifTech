8f444cb4911dfb06014869fad717c84b279a2c4d
iavazov <CSharpLesson>
2021-11-02 19:57:01 +0500

    added class and void for generate hmac-sha1

---
+++ ProjectForAlifTech/Helper/GenerateHash.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronWallet.Helper
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class GenerateHash
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="signatureString"></param>
+        /// <returns></returns>
+        public static string hash_hmac(string signatureString)
+        {
+            string secretKey = "aliftech";
+            HMACSHA1 hmac = new HMACSHA1(Encoding.UTF8.GetBytes(secretKey));
+            byte[] buffer = Encoding.UTF8.GetBytes(signatureString);
+            string result = BitConverter.ToString(hmac.ComputeHash(buffer)).Replace("-", "").ToLower();
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(result));
+        }
+    }
+}
