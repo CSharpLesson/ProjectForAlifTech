f5795c02d1745f66e2abe8972827336976316da0
iavazov <CSharpLesson>
2021-11-02 07:37:29 +0500

    added User Account model

--- ProjectForAlifTech/DataContext/DataContext.cs
+++ ProjectForAlifTech/DataContext/DataContext.cs
@@ -10,7 +10,7 @@ namespace ElectronWaller.DataContexts
         /// <summary>
         /// 
         /// </summary>
-        public static string ConnectionString { get; set; }
+        public static string ConnectionString { get; set; }        
 
         /// <summary>
         /// 
---
+++ ProjectForAlifTech/Models/UserAccount.cs
@@ -0,0 +1,35 @@
+﻿using ElectronWaller.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.Serialization;
+
+namespace ElectronWallet.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Table("user_account")]
+    public class UserAccount
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("user_id")]
+        [ForeignKey(nameof(UserModel))]
+        public int UserId { get; set; }
+
+        [IgnoreDataMember]
+        public virtual User UserModel { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("balance")]
+        public decimal Balance { get; set; }
+    }
+}
