0ea9ed998e2bfdd7dab976f1628827323ee160cd
iavazov <CSharpLesson>
2021-11-02 07:06:13 +0500

    Added User Model Class

---
+++ ProjectForAlifTech/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectForAlifTech.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Table("user")]
+    public class User
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("x-user_id")]
+        public string XUserId { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column("phone")]
+        public string Phone { get; set; }
+    }
+}
